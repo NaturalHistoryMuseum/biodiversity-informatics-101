90bdd6da5832e49c13f13334cf6825b996d7ff37
Ben Scott <ben@benscott.co.uk>
2020-01-30 20:15:30 +0000

    Add helpers and update notebook

---
+++ helpers.py
@@ -0,0 +1,10 @@
+import base64
+from IPython.display import HTML
+
+def create_download_link( df, title = "Download CSV file", filename = "data.csv"):
+    csv = df.to_csv()
+    b64 = base64.b64encode(csv.encode())
+    payload = b64.decode()
+    html = '<a download="{filename}" href="data:text/csv;base64,{payload}" target="_blank">{title}</a>'
+    html = html.format(payload=payload,title=title,filename=filename)
+    return HTML(html)
